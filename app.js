bb118531b9e3b6a96fa0043f679027383cf40b1f
Arun Mopare <arunmopare546@gmail.com>
2022-06-04 22:21:28 +0530

    initialized project

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello Clothes!'))
+app.listen(port, () => console.log(`app listening on port ${port}!`))
